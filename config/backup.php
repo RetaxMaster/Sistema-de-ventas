541fcd84b4573fb91b58c9f2d7a8478ac0deb679
RetaxMaster <retaxmaster@hotmail.com>
2019-08-08 13:21:51 -0500

    Configuracion final de Google Drive

--- config/backup.php
+++ config/backup.php
@@ -8,7 +8,7 @@ return [
          * The name of this application. You can use this name to monitor
          * the backups.
          */
-        'name' => env('GOOGLE_DRIVE_FOLDER_ID', 'laravel-backup'),
+        'name' => "",
 
         'source' => [
 
